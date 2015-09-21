778e8b070c3395e87d7f641a052079d20834b92b
Kyle Chapman <xeuorux@gmail.com>
2015-09-21 11:27:27 -0700

    Redone PowerOfTwo solution
    Redid the solution to PowerOfTwo with bitwise operations. Much better
    now.

--- src/PowerOfTwo.java
+++ src/PowerOfTwo.java
@@ -1,20 +1,28 @@
-//a simple if bad solution
-//just divide it by two until you get to 0, 1, or a number that isn't disivible by 2
-//to determine if the number is a power of two
-
-//i saw some bitwise solutions to this, might try those out eventually
+//this is a much much better solution than the one i had on here before
+//i had to learn a lot more about bit operations and become more comfortable using them
+//before this kind of solution could make sense to me
+//really happy with the result
 
 public class PowerOfTwo
 {
 	public boolean isPowerOfTwo(int n) {
-        while (n > 0)
+        boolean hasFoundOne = false;
+        while (n != 0) //if the number is zero its either zero or a power of two
         {
-            if (n == 1)
-                return true;
-            if (n % 2 != 0)
-                return false;
-            n /= 2;
+            if ((n & 1) == 1) //if the rightmost bit is 1
+            {
+            	//if weve already seen a 1 bit, then it cant be a power of two
+                if (hasFoundOne)
+                    return false;
+                hasFoundOne = true;
+            }
+            n = n >> 1; //bitshift right by 1
         }
-        return false;
+        return hasFoundOne;
+        //when the number becomes zero its either zero
+        //in which case hasFoundOne is false and so returns false (the correct answer for zero)
+        //or its some number that has some number of leading zeroes before its first non-zero value
+        //which means that it is a power of two
+        //and therefore hasFoundOne is true so returns true
     }
 }
